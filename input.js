820706ad9d1445245bdffa9eebc1f9325ace8a54
Koji Eguchi <koji.eguchi@hotmail.com>
2023-02-15 14:28:33 -0500

    modified to accept inputs from command line

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -10,6 +13,18 @@ const setupInput = function() {
 const handleUserInput = function(data) {
   if (data === '\u0003') {
     process.exit();
+  }
+  if (data === '\u0077') {
+    connection.write("Move: up");
+  }
+  if (data === '\u0061') {
+    connection.write("Move: left");
+  }
+  if (data === '\u0073') {
+    connection.write("Move: down");
+  }
+  if (data === '\u0064') {
+    connection.write("Move: right");
   }
 };
 
